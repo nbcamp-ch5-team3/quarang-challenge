15eb2e60a11ae3a1a7c98b7bdc84bbe6703d8567
QuaRang <dbduddnd1225@gmail.com>
2025-05-07 14:32:32 +0900

    chore: 프로젝트 환경세팅

---
+++ quarang-challenge/Presentation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  quarang-challenge
+//
+//  Created by Quarang on 5/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ quarang-challengeTests/quarang_challengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  quarang_challengeTests.swift
+//  quarang-challengeTests
+//
+//  Created by Quarang on 5/7/25.
+//
+
+import Testing
+@testable import quarang_challenge
+
+struct quarang_challengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
