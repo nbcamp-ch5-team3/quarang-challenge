ca8efacca7f9422fc30205089036f2af7405a716
QuaRang1225 <dbduddnd1225@gmail.com>
2025-05-08 17:36:35 +0900

    임시

---
+++ Data/Sources/Repository/FetchMusicRepository.swift
@@ -0,0 +1,13 @@
+//
+//  FetchMusicRepository.swift
+//  Data
+//
+//  Created by Quarang on 5/8/25.
+//
+
+import Foundation
+import Domain
+
+public final class FetchMusicRepository: FetchMusicRepositoryInterface {
+    public init() { }
+}
--- quarang-challenge/Domain/111.swift
+++
@@ -1,8 +0,0 @@
-//
-//  111.swift
-//  quarang-challenge
-//
-//  Created by Quarang on 5/8/25.
-//
-
-import Foundation
